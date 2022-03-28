edd551d47e53728ae70369dc846117fdb6db3f66
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2022-03-28 07:15:12 +0000

    Added basic template and redirect to index

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from job_offers import urls as job_offers__urls
 
 urlpatterns = [
+    path('', include(job_offers__urls)),
     path('admin/', admin.site.urls),
 ]
