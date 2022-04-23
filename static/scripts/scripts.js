b74906d6d75b30e00d1ecb4b4090e10ac811ff62
maciejowoc <101329643+maciejowoc@users.noreply.github.com>
2022-04-23 13:55:16 +0200

    feat(Pop-up window): Pop-up window is working - now it needs to be styled

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -16,6 +16,7 @@ darkModeSwitcher.onclick = () => {
   }
 };
 
+
 const meatballsMenuButton = document.getElementsByClassName(
   "nav-item meatballs-menu"
 )[0];
@@ -31,4 +32,22 @@ meatballsMenuButton.onclick = (evt) => {
 meatballsMenuContent.onclick = (evt) => {
   //prevent closing menu when clicking on content
   evt.stopPropagation();
+} 
+
+
+//_____________________Pop-up window_________________________
+
+var modal = document.getElementsByClassName("modal")[0];
+var CloseBtn = document.getElementsByClassName("SwitchOff")[0];
+
+CloseBtn.onclick = function() {
+  modal.style.display = "none";
 }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
