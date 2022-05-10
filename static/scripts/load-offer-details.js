336519274ee973c33520d4d8a58127fb4f35aeb2
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2022-05-10 03:33:16 +0200

    Added JS script for updating offer details and imported it in index

---
+++ static/scripts/load-offer-details.js
@@ -0,0 +1,44 @@
+const offerDetailsDiv = document.querySelector(".appointmentDetails");
+
+
+const detailsTags = offerDetailsDiv.querySelector(".detailsTags");
+const detailsLogoImg = offerDetailsDiv.querySelector(".detailsLogo img");
+const detailsPositionJob = offerDetailsDiv.querySelector(".detailsPositionJob");
+const detailsCompanyName = offerDetailsDiv.querySelector(".detailsCompanyName");
+const detailsSalary = offerDetailsDiv.querySelector(".detailsSalary");
+
+const detailsContractType = offerDetailsDiv.querySelector(".detailsContractType")
+const detailsWorkTime = offerDetailsDiv.querySelector(".detailsWorkTime")
+const detailsWorkMode = offerDetailsDiv.querySelector(".detailsWorkMode")
+const detailsCity = offerDetailsDiv.querySelector(".detailsCity")
+
+const detailsPositionDescription = offerDetailsDiv.querySelector(".detailsPositionDescription");
+const detailsCompanyDescription = offerDetailsDiv.querySelector(".detailsCompanyDescription");
+
+
+document.addEventListener("click", async function(event) {
+    if (!event.target.classList.contains("offer__show-details-button")) return;
+
+    const offerToLoadId = event.target.dataset.offerId
+
+    response = await fetch(`/api/offer-details?id=${offerToLoadId}`);
+    responseData = await response.json();
+
+    if (responseData.status !== "success") return;
+
+    detailsTags.innerText = responseData.data.tags.map(tag => tag.text).join(", ");
+    detailsLogoImg.src = responseData.data.company.logo.url;
+    detailsPositionJob.innerText = responseData.data.position
+    detailsCompanyName.innerText = responseData.data.company.name
+    detailsSalary.innerText = `${responseData.data.salary_min}-${responseData.data.salary_max} zł`
+
+    detailsContractType.innerText = responseData.data.contract_type
+    detailsWorkTime.innerText = responseData.data.work_time
+    detailsWorkMode.innerText = responseData.data.work_mode
+    detailsCity.innerText = responseData.data.city
+
+    detailsPositionDescription.innerText = responseData.data.description
+    detailsCompanyDescription.innerText = responseData.data.company.description
+
+    offerDetailsDiv.style.display = "block";
+}, true);
